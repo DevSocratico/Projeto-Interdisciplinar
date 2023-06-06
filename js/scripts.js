2e1e178bd7c1c3a2b8f3b6bff348d88bb6ac9a5d
DevSocratico <lukinha_araraquara@hotmail.com>
2023-06-06 15:56:21 -0300

    Adicionado modal de cadastro

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+function abrirModalCadastro() {
+    const cadastroModal = new bootstrap.Modal(document.getElementById('cadastroModal'));
+    cadastroModal.show();
+}
+
+function abrirModalCadastroContaPessoal() {
+    const cadastroContaPessoalModal = new bootstrap.Modal(document.getElementById('cadastroContaPessoalModal'));
+    cadastroContaPessoalModal.show();
+}
+
+function abrirModalCadastroFarmacia() {
+    const cadastroFarmaciaModal = new bootstrap.Modal(document.getElementById('cadastroFarmaciaModal'));
+    cadastroFarmaciaModal.show();
+}
