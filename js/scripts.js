94ebf56eb24f4a52a788d2b85a1fa3a89b7e794b
DevSocratico <lukinha_araraquara@hotmail.com>
2023-06-11 20:56:37 -0300

    Adicionado modo noturno

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Modal Cadastro
 function abrirModalCadastro() {
     const cadastroModal = new bootstrap.Modal(document.getElementById('cadastroModal'));
     cadastroModal.show();
@@ -12,3 +13,31 @@ function abrirModalCadastroFarmacia() {
     const cadastroFarmaciaModal = new bootstrap.Modal(document.getElementById('cadastroFarmaciaModal'));
     cadastroFarmaciaModal.show();
 }
+
+// Modo Noturno
+const botaoDark = document.getElementById('modo-noturno');
+let theme = localStorage.getItem('data-theme');
+
+if (theme == 'dark') {
+    botaoDark.checked = true;
+}
+
+botaoDark.addEventListener('change', () => {
+    if (botaoDark.checked) {
+        ativarModoNoturno();
+    } else {
+        ativarModoClaro();
+    }
+});
+
+const ativarModoNoturno = () => {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("data-theme", "dark");
+}
+
+const ativarModoClaro = () => {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("data-theme", 'light');
+}
+
+if (theme == 'dark') ativarModoNoturno();
